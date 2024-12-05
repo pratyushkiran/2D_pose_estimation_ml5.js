c345be86af3a75b674c2385ddfc8e68fb6d63a7f
Pratyush Kiran Panda <124147747+pratyushkiran@users.noreply.github.com>
2024-12-05 11:44:25 +0530

    project created

---
+++ sketch.js
@@ -0,0 +1,13 @@
+function setup() {
+  createCanvas(innerWidth, innerHeight);
+}
+
+function draw() {
+  background(255, 0, 0, 2
+
+  );
+
+  fill("white")
+  noStroke();
+  circle(mouseX, mouseY, 200)
+}
