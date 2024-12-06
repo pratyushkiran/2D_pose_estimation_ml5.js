508f40ba5278d109aad768c50313e83e1e20f87f
Pratyush Kiran Panda <124147747+pratyushkiran@users.noreply.github.com>
2024-12-06 11:41:26 +0530

    commit

--- sketch.js
+++ sketch.js
@@ -25,11 +25,17 @@ function setup() {
   video.hide();
 
   bodyPose.detectStart(video, gotPoses);
+  connections = bodyPose.getSkeleton();
+  console.log(connections);
 }
 
 function draw() {
+  
   image(video, 0, 0);
+
   jokerNose();
+
+  // for detecting all the keypoints
   if (poses.length > 0) {
     let pose = poses[0];
     for (let i = 0; i < pose.keypoints.length; i++) {
@@ -40,15 +46,27 @@ function draw() {
         circle(keypoint.x, keypoint.y, 12);
       }
     }  
+    // for (let i = 0; i < connections.length(); i++) {
+    //   let connection = connections[i];
+    //   let a = connection[0];
+    //   let b = connection[1];
+    //   let keyPointA = pose.keypoints[a];
+    //   let keyPointB = pose.keypoints[b];
+    //   stroke(0, 255, 0);
+    //   strokeWeight(8);
+    //   line(keyPointA.x, keyPointA.y, keyPointB.x, keyPointB.y);
+    // }
   }
 }
 
-function jokerNose() {
+function jokerNose(fun = null) {
   // Draw keypoints if pose detection has started
   if (poses.length > 0) {
     let pose = poses[0];
+
     let x = pose.nose.x;
     let y = pose.nose.y;
+
     let lerpAmount = 0.3;
 
     // Interpolate between previous and current positions
@@ -58,19 +76,9 @@ function jokerNose() {
     // Draw the interpolated position
     fill(255, 0, 0);
     circle(lerpedX, lerpedY, 40); // smoothened keypoints
-    // circle(smoothenKeypoints(x, lerpAmount), smoothenKeypoints(y, lerpAmount), 40);
-    // stroke(255, 255, 0 , 255);
-    // circle(x, y, 10);
-
+    
     // Update previous positions
     prevX = lerpedX;
     prevY = lerpedY;
   }
 }
-
-// function smoothenKeypoints(x, lerpAmount) {
-//   let prevX = 0;
-//   let lerpedX = lerp(prevX, x, lerpAmount);
-//   return lerpedX;
-  
-// }
