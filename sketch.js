9cf50f67c6d5ee14fb4e68399e3e720216fba965
Pratyush Kiran Panda <124147747+pratyushkiran@users.noreply.github.com>
2024-12-06 13:03:25 +0530

    pose estimation and connecting lines implemeted with confidence filter

--- sketch.js
+++ sketch.js
@@ -20,8 +20,8 @@ function mousePressed() {
 }
 
 function setup() {
-  createCanvas(innerWidth, innerHeight, WEBGL);
-  video = createCapture(VIDEO, { flipped:true });
+  createCanvas(innerWidth, innerHeight);
+  video = createCapture(VIDEO, { flipped:false });
   video.hide();
 
   bodyPose.detectStart(video, gotPoses);
@@ -30,44 +30,18 @@ function setup() {
 }
 
 function draw() {
-  
+s
   image(video, 0, 0);
 
-  jokerNose();
 
-  // for detecting all the keypoints
   if (poses.length > 0) {
-    let pose = poses[0];
-    for (let i = 0; i < pose.keypoints.length; i++) {
-      let keypoint = pose.keypoints[i];
-      fill(0, 255, 0); 
-      noStroke();
-      if (keypoint.confidence > 0.3) {
-        circle(keypoint.x, keypoint.y, 12);
-      }
-    }  
-    // for (let i = 0; i < connections.length(); i++) {
-    //   let connection = connections[i];
-    //   let a = connection[0];
-    //   let b = connection[1];
-    //   let keyPointA = pose.keypoints[a];
-    //   let keyPointB = pose.keypoints[b];
-    //   stroke(0, 255, 0);
-    //   strokeWeight(8);
-    //   line(keyPointA.x, keyPointA.y, keyPointB.x, keyPointB.y);
-    // }
-  }
-}
 
-function jokerNose(fun = null) {
-  // Draw keypoints if pose detection has started
-  if (poses.length > 0) {
     let pose = poses[0];
 
     let x = pose.nose.x;
     let y = pose.nose.y;
 
-    let lerpAmount = 0.3;
+    let lerpAmount = 0.4;
 
     // Interpolate between previous and current positions
     let lerpedX = lerp(prevX, x, lerpAmount); // Adjust the last parameter (0.1) for smoother/slower interpolation
@@ -80,5 +54,60 @@ function jokerNose(fun = null) {
     // Update previous positions
     prevX = lerpedX;
     prevY = lerpedY;
+
+    // for detecting all the keypoints
+
+    for (let i = 0; i < pose.keypoints.length; i++) {
+      let keypoint = pose.keypoints[i];
+      fill(0, 255, 0); 
+      noStroke();
+      if (keypoint.confidence > 0.3) {
+        circle(keypoint.x, keypoint.y, 12);
+      }
+    }  
+
+    // for connecting lines
+
+    for (let i = 0; i < connections.length; i++) {
+      let connection = connections[i];
+      let a = connection[0];
+      let b = connection[1];
+      let keyPointA = pose.keypoints[a];
+      let keyPointB = pose.keypoints[b];
+
+      let confA = keyPointA.confidence;
+      let confB = keyPointB.confidence;
+
+      // if (confA > 0.1 && confB > 0.1) {
+      stroke(0, 255, 0);
+      strokeWeight(4);
+      line(keyPointA.x, keyPointA.y, keyPointB.x, keyPointB.y);
+      // }
+    }
+
+
+
+
+    // ================================================================= 
+    // detect the left wrist and the right wrist and draw the nose radius according to the distance between two wrists
+    // let rx = pose.right_wrist.x;
+    // let ry = pose.right_wrist.y;
+
+    // let lx = pose.left_wrist.x;
+    // let ly = pose.left_wrist.y;
+
+    // fill(0, 255, 0); 
+    // circle(rx, ry, 12);
+    
+    // fill(0, 150, 0); 
+    // circle(lx, ly, 12);
+
+    // let d = dist(rx, ry, lx, ly);
+    // fill(255, 0, 0);
+    // circle(x, y, d);
+    // ================================================================= 
+
+    
   }
 }
+
